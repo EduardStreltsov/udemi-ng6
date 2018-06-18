98d376d1bcb2009320007ae99a901e5450f32fcd
eduard.streltsov <streltsov.eduard@gmail.com>
2018-06-18 16:20:26 +0300

    till lecture 55 incl

--- src/app/recipe-book/recipe-list/recipe-list.component.ts
+++ src/app/recipe-book/recipe-list/recipe-list.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {RecipeBook} from "../recipe-book.model";
+import {RecipeBook} from "../recipe.model";
 
 @Component({
   selector: 'app-recipe-list',
@@ -9,7 +9,8 @@ import {RecipeBook} from "../recipe-book.model";
 export class RecipeListComponent implements OnInit {
 
   recipes: RecipeBook[] = [
-    new RecipeBook('1 recipe', )
+    new RecipeBook('1 recipe', '1 recipe desc', 'https://www.wellplated.com/wp-content/uploads/2017/12/Hoppin-John-recipe-600x629.jpg'),
+    new RecipeBook('2 recipe', '2 recipe desc', 'https://www.wellplated.com/wp-content/uploads/2017/12/Hoppin-John-recipe-600x629.jpg')
   ];
 
   constructor() {
